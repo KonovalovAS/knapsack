59e140704019f6014b49db56794bb398f96a846e
Konovalov Andrey Sergeevich <andrrrey.konovalov@gmail.com>
2022-04-20 15:25:47 +0300

    Initial solution seek

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,19 +1,28 @@
 #include "knapsack.h"
 
+int Problem::item::ID = 0;
+
 Problem::item::item( int w0, int v0 ){
     val = v0;
     w = w0;
+    spec_val = (w0>0) ? (double)v0/w0 : -1 ;
+    id = ID;
+    ID ++;
 }
+
+bool Problem::item::operator<( const item &right ){
+    return this->spec_val > right.spec_val;
+}
+
+// Problem
 
 Problem::Problem( int K_arg, vector<item> &I ){
     n = I.size();
     K = K_arg;
     Items = I;
-}
-
-void Problem::add_item( item &newitem ){
-    n++;
-    Items.push_back(newitem);
+    taken = vector<bool> (n, false);
+    sol_value = 0;
+    sol_weight = 0;
 }
 
 Problem::item Problem::operator[](int k){
@@ -24,29 +33,42 @@ Problem::item Problem::operator[](int k){
     }
 }
 
-// Solution
+// Solving
 
-Problem::Solution::Solution( Problem *p ){
-    problem = p;
+void Problem::Solve(){
+
+    sort(Items.begin(),Items.end());
+
+    int i = 0;
+    bool keep_going = true;
+    while( keep_going && (i < n) ){
+
+        if( sol_weight + Items[i].w <= K ){
+            taken[ Items[i].id ] = true;
+            sol_weight += Items[i].w;
+            sol_value += Items[i].val;
+        }
+        i++;
+    }
+
+};
+
+void Problem::show_items(){
+    for(auto i: Items)
+        cout<<"( "<<i.id<<", "<<i.spec_val<<" ) ";
 }
 
-Problem::Solution::Solution( Problem *p, vector<int> &init ){
-    problem = p;
-    content = init;
+void Problem::show_taken(){
+    for(auto f: taken)
+        cout << f << " ";
 }
 
-int Problem::Solution::weight(){
-    int wght(0);
-    for(int k=0; k<problem->n; k++)
-        wght += ((*problem)[k]).w * content[k];
-    return wght;
+int Problem::Solution_value(){
+    return sol_value;
 }
 
-int Problem::Solution::value(){
-    int val(0);
-    for(int k=0; k<problem->n; k++)
-        val += ((*problem)[k]).val * content[k];
-    return val;
+int Problem::Solution_weight(){
+    return sol_weight;
 }
 
 
--- knapsack.h
+++ knapsack.h
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Problem{
 public:
     struct item{
+        int id;
         int val,w;
+        double spec_val;
+        static int ID;
+
         item( int w0=0, int v0=0 );
+        bool operator<( const item &right );
     };
 
     Problem( int K_arg, vector<item> &I );
-    void add_item( item &newitem );
+
+    void Solve();
+
     item operator[](int k);
+
+    void show_taken();
+    void show_items();
+
+    int Solution_value();
+    int Solution_weight();
 
 private:
     int n,K;
     vector<item> Items;
 
-    class Solution{
-    private:
-        vector<int> content;
-        Problem *problem;
-    public:
-        Solution( Problem *p );
-        Solution( Problem *p, vector<int> &init );
-        int weight();
-        int value();
-    };
+    int sol_weight, sol_value;
+    vector<bool> taken;
 };
